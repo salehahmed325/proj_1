ff10146cbfac8c9830454be2bdc5133048d3e32d
salehahmed325 <salehahmed904@gmail.com>
2026-04-30 12:42:01 +0600

    code reformat

--- calc/views.py
+++ calc/views.py
@@ -3,13 +3,15 @@ from os import name
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html',{'name':'saleh'})
+    return render(request, 'index.html', {'name': 'saleh'})
+
 
 def add(request):
     val1 = int(request.GET["num1"])
     val2 = int(request.GET["num2"])
     res = val1 + val2
-    return render(request, 'result.html',{'result':res})
+    return render(request, 'result.html', {'result': res})
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-sw7&8*+^oumhq!a9l$6^6*f=kx0!yz3=e1*1+=j+2)%w9)d1-m
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -68,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
@@ -78,7 +75,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -98,7 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -109,7 +104,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
