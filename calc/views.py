3525afd7a507ad86ace0d4c37af0ab54e078bcb5
salehahmed325 <salehahmed904@gmail.com>
2026-04-30 12:37:52 +0600

    added form to add two numbers

--- calc/views.py
+++ calc/views.py
@@ -7,3 +7,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html',{'name':'saleh'})
+
+def add(request):
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    res = val1 + val2
+    return render(request, 'result.html',{'result':res})
