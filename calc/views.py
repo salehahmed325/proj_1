dcadb0de17ffd08650a8bd1168de089a6322b4fd
salehahmed325 <salehahmed904@gmail.com>
2026-04-30 11:13:38 +0600

    added base.html file

--- calc/views.py
+++ calc/views.py
@@ -1,7 +1,9 @@
+from os import name
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'name':'saleh'})
