c3a40053d11e99e754c4e4837d2251a96e47e52d
salehahmed325 <salehahmed904@gmail.com>
2026-05-06 13:01:15 +0600

    created registration page and form

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('calc.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
