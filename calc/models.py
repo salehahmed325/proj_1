b5cb979ba2d1665464e5531d092dab10aa57ee4f
salehahmed325 <salehahmed904@gmail.com>
2026-05-06 11:37:57 +0600

    added pgsql db and create a table(Destination)

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db import models  # type: ignore[import-untyped]
 
 # Create your models here.
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='images/')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
